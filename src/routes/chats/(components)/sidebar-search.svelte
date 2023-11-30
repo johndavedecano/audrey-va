d082c6647ba0c413924112fe15fe11cedf7593b9
John Dave Decano <johndavedecano@gmail.com>
2023-11-30 06:31:02 +0100

    update search

--- src/routes/chats/(components)/sidebar-search.svelte
+++ src/routes/chats/(components)/sidebar-search.svelte
@@ -1,17 +1,43 @@
 <script>
   // @ts-nocheck
 
-  import { IconFilter, IconSearch } from "@tabler/icons-svelte";
+  import { IconFilter, IconSearch, IconX } from "@tabler/icons-svelte";
+
+  let value = "";
+
+  const onFilter = () => {};
+
+  const onClear = () => {
+    value = "";
+  };
 </script>
 
 <div class="sidebar-search text-gray-600">
-  <IconSearch size={16} />
+  <a
+    href="/dashboard"
+    on:click|preventDefault={onClear}
+    class="btn btn-link"
+    title="Change Status"
+  >
+    {#if value}
+      <IconX size={16} />
+    {:else}
+      <IconSearch size={16} />
+    {/if}
+  </a>
+
   <input
     type="text"
     placeholder="Search Chats"
     class="input input-sm w-full max-w-xs"
+    bind:value
   />
-  <a href="/dashboard" class="btn btn-link" title="Change Status">
+  <a
+    href="/dashboard"
+    on:click|preventDefault={onFilter}
+    class="btn btn-link"
+    title="Change Status"
+  >
     <IconFilter size={16} />
   </a>
 </div>
